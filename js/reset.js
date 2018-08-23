7deb1b502f27051447a7e47f7a25d15a575969e2
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2018-08-23 14:57:48 -0300

    Parte visual finalizada. Necessario lapidações e rafatoramento. Add reset.js para resetar dados no backend

---
+++ js/reset.js
@@ -0,0 +1,9 @@
+
+
+const services = {
+"webDevelopment": "Descrição em algumas palavras sobre o serviço de desenvolvimento Web.",
+
+"mobileApps": "Descrição em algumas palavras sobre o serviço de desenvolvimento Mobile.",
+
+"systemsDevelopment": "Descrição em algumas palavras sobre o serviço de desenvolvimento e aprimoramento de Sistemas."
+}
