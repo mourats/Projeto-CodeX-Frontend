28e9c08b575ec71a241184c6c26d1af140512cf5
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2018-08-24 19:06:26 -0300

    Subindo painel do controle do administrador e algumas alterações

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope, $http) {
+
+  $http.get('https://codex-api.herokuapp.com/', {
+headers: {'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='}
+}).then(function(response) {}, function(response) {
+        console.log(response.data.status)
+    });
+});
